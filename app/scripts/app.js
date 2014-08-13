d6db7db38e30641ba8213982f310443649c9e759
Troy Scott <troys2005@gmail.com>
2014-08-13 15:51:36 -0700

    chapter 4 - update

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+var RocknrollcallYeoman = window.RocknrollcallYeoman = Ember.Application.create();
+
+/* Order and include as you please. */
+require('scripts/controllers/*');
+require('scripts/store');
+require('scripts/models/*');
+require('scripts/routes/*');
+require('scripts/components/*');
+require('scripts/views/*');
+require('scripts/router');
+
+RocknrollcallYeoman.applicationName = "Rock 'n' Roll Call";
+
