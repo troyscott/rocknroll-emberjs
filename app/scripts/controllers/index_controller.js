1f83305b49f340cb93ef6993b7d8fd53b60d5dd1
Troy Scott <troys2005@gmail.com>
2014-08-14 17:59:41 -0700

    Chapter 4 - added actiont to IndexController

--- app/scripts/controllers/index_controller.js
+++ app/scripts/controllers/index_controller.js
@@ -1,2 +1,14 @@
-RocknrollcallYeoman.IndexController = Ember.Controller.extend({});
+RocknrollcallYeoman.IndexController = Ember.Controller.extend({
+  actions: {
+   viewArtists: function(artist) {
+      console.log('hang on ... viewing: ' + artist.name);
+     
+   }
+    
+    
+  }
+
+
+
+});
 
